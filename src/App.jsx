0100bbf55ecdfd9629fc8cc990bd068c4feb42fc
mubashirbappi <mubashirbappi@gmail.com>
2024-10-22 16:52:17 +0600

    setup all

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import Header from './component/Header'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    <h1 className='text-2xl font-bold text-red-500'>this is setup time</h1>
+    
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <div>
+            <p>header</p>
+            
+        </div>
+    );
+};
+
+export default Header;
